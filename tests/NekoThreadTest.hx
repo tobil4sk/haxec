50cf8abe8aee1a4a23c2a99fb464c3701688768c
tobil4sk <tobil4sk@outlook.com>
2023-04-04 03:04:02 +0100

    Copy from haxelib

--- tests/NekoThreadTest.hx
+++ tests/NekoThreadTest.hx
@@ -1,13 +1,27 @@
 import sys.thread.Lock;
+import sys.thread.Thread;
 
 function main() {
-	final lock = new Lock();
+	final p = new sys.io.Process("git", ["subodule", "update"]);
+	// just in case process hangs waiting for stdin
+	p.stdin.close();
 
-	sys.thread.Thread.create(function() {
-		trace("Hello world");
-		Sys.sleep(0.2);
-		lock.release();
-	});
-	trace("Hi world");
-	lock.wait();
+	final streamsLock = new Lock();
+	function readFrom(stream:haxe.io.Input, to: {value: String}) {
+		to.value = stream.readAll().toString();
+		streamsLock.release();
+	}
+
+	final out = {value: ""};
+	final err = {value: ""};
+	Thread.create(readFrom.bind(p.stdout, out));
+	Thread.create(readFrom.bind(p.stderr, err));
+
+	final code = p.exitCode();
+	for (_ in 0...2) {
+		// wait until we finish reading from both streams
+		streamsLock.wait();
+	}
+
+	p.close();
 }
